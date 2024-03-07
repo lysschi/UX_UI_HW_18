1a1cb6e67f466108563854a2e09b3736220c0598
lysschi <lysschi@gmail.com>
2024-03-06 23:56:08 -0600

    css html js
    css html js

--- js/index.js
+++ js/index.js
@@ -1,2 +1,17 @@
 
 console.log("Your index.js file is loaded correctly!");
+$('#myform').on('submit', function(ev) {
+    $('#my-modal').modal({
+        show: 'false'
+    }); 
+
+
+    var data = $(this).serializeObject();
+    json_data = JSON.stringify(data);
+    $("#results").text(json_data); 
+    $(".modal-body").text(json_data); 
+
+    // $("#results").text(data);
+
+    ev.preventDefault();
+});
